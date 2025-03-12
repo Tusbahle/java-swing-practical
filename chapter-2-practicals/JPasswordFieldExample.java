c308f87a8a1eb0a5fcc4d47fae50abf85755906b
Abdirahman Tusbahle <23212614+Tusbahle@users.noreply.github.com>
2025-03-12 14:08:25 +0300

    bug fix

--- chapter-2-practicals/JPasswordFieldExample.java
+++ chapter-2-practicals/JPasswordFieldExample.java
@@ -1,9 +1,13 @@
-schoolName = 'Najax School'; 
-studentName = "Abdirahman Abdirazak";
-print("School Name:", schoolName);
-print("Student Name:", studentName);
- #calculation
- num1 = 20;
- num2 = 10;
- #addition
- print(num1 + num2);
+import javax.swing.JFrame;
+import javax.swing.JPasswordField;
+public class JPasswordFeidlExample{
+    public static void main(String options[]){
+        JFrame myForm = new JFrame();
+        myForm.setDefaultCloseOperation(2);
+        myForm.setSize(200, 300);
+        JPasswordField myPassword = new JPasswordField();
+        myPassword.setText("Save button 1");
+        myForm.add(myPassword);
+        myForm.setVisible(true);
+    }
+}
